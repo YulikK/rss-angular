521fbc5e8095d214f0de93b4e88e60c1077ee6da
YulikK <yulikpoch@gmail.com>
2024-10-18 19:58:39 +0200

    ⚙️ chore: install eslint and prettier

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'temp-angular-project' title`, () => {
+  it("should have the 'temp-angular-project' title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('temp-angular-project');
