e8feb85b022b77c637145ab7c53fda34250b9083
YulikK <yulikpoch@gmail.com>
2024-10-19 15:58:59 +0200

    🪄 refactor: delete import Common module

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,13 +1,12 @@
 import { YouTubeVideo } from '@/shared/types';
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { SearchService } from './search.service';
 import { ItemComponent } from './item/item.component';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [ItemComponent, CommonModule],
+  imports: [ItemComponent],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
 })
