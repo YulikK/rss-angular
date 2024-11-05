c8f69656e4772c7c25718cfa15a9a3da13c5cdec
YulikK <yulikpoch@gmail.com>
2024-11-05 09:30:29 +0100

    🪄 refactor: add rout constant

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,27 @@
 import { Routes } from '@angular/router';
+import { ROUTES } from '@/shared/constant';
 import { SearchListComponent } from './youtube/components/search-list/search-list.component';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', title: 'home', component: SearchListComponent, canActivate: [authGuard] },
+  { path: ROUTES.EMPTY, pathMatch: 'full', redirectTo: ROUTES.HOME },
+  { path: ROUTES.HOME, title: ROUTES.HOME, component: SearchListComponent, canActivate: [authGuard] },
   {
-    path: 'details/:id',
-    title: 'details',
+    path: ROUTES.DETAILS,
+    title: ROUTES.DETAILS,
     loadComponent: () => import('./youtube/pages/details/details.component').then((m) => m.DetailsComponent),
     canActivate: [authGuard],
   },
   {
-    path: 'login',
-    title: 'login',
+    path: ROUTES.LOGIN,
+    title: ROUTES.LOGIN,
     loadComponent: () => import('./core/pages/login/login.component').then((m) => m.LoginComponent),
   },
   {
-    path: 'not-found',
-    title: '404',
+    path: ROUTES.NOT_FOUND,
+    title: ROUTES.NOT_FOUND,
     loadComponent: () => import('./core/pages/not-found/not-found.component').then((m) => m.NotFoundComponent),
   },
-  { path: '**', redirectTo: '/not-found' },
+
+  { path: ROUTES.NO_MATCH, redirectTo: ROUTES.NOT_FOUND },
 ];
---
+++ src/shared/constant.ts
@@ -0,0 +1,11 @@
+export const ROUTES = {
+  EMPTY: '',
+  HOME: 'home',
+  LOGIN: 'login',
+  DETAILS: 'details/:id',
+  NOT_FOUND: 'not-found',
+  NO_MATCH: '**',
+  REGISTER: '/register',
+  SETTINGS: '/settings',
+  WATCH: '/watch/:id',
+} as const;
